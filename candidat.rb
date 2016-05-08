0b19563fa3064b5c6ecb999c6bf9d683b3575bb8
Thibauld Favre <tfavre@gmail.com>
2016-05-08 13:27:54 -0400

    * Fix xss security issues

--- candidat.rb
+++ candidat.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require 'date'
 require 'erb'
 include ERB::Util
 
@@ -33,7 +34,9 @@ SELECT c.*, CASE WHEN s.soutiens is NULL THEN 0 ELSE s.soutiens END
   on (s.candidate_id = c.candidate_id)
 WHERE c.candidate_key = $1;
 END
-
+			@queries['get_supporters_by_key']=<<END
+select c.firstname, c.lastname, c.city, ci.departement, s.support_date from citizens as c inner join supporters as s on (s.user_id=c.user_id) inner join candidates as ca on (ca.candidate_id=s.candidate_id) left join cities as ci on (ci.city_id=c.city_id) where ca.candidate_key=$1;
+END
 		end
 		def self.db_init
 			Candidat.db=PG.connect(
@@ -75,6 +78,33 @@ END
 			candidat['encoded_name']=URI::encode(candidat['name'])
 			candidat['goal']=candidat['soutiens'].to_i<=500 ? 500 : candidat['soutiens']
 			candidat['qualified']= (candidat['soutiens'].to_i >= 500)
+			secteur=html_escape(candidat['secteur']) unless candidat['secteur'].nil?
+			departement_name=candidat['departement'].split(' - ')[1] unless candidat['departement'].nil?
+			departement_name=html_escape(departement_name) unless departement_name.nil?
+			departement=html_escape(candidat['departement']) unless candidat['departement'].nil?
+			job=html_escape(candidat['job']) unless candidat['job'].nil?
+			vision=html_escape(candidat['vision']) unless candidat['vision'].nil?
+			prio1=html_escape(candidat['prio1']) unless candidat['prio1'].nil?
+			prio2=html_escape(candidat['prio2']) unless candidat['prio2'].nil?
+			prio3=html_escape(candidat['prio3']) unless candidat['prio3'].nil?
+			trello=html_escape(candidat['trello'].split('?')[0]) unless candidat['trello'].nil? or candidat['trello'].empty?
+			website=html_escape(candidat['website'].split('?')[0]) unless candidat['website'].nil? or candidat['website'].empty?
+			facebook=html_escape(candidat['facebook'].split('?')[0]) unless candidat['facebook'].nil? or candidat['facebook'].empty?
+			twitter=html_escape(candidat['twitter'].split('?')[0]) unless candidat['twitter'].nil? or candidat['twitter'].empty?
+			linkedin=html_escape(candidat['linkedin'].split('?')[0]) unless candidat['linkedin'].nil? or candidat['linkedin'].empty?
+			blog=html_escape(candidat['blog'].split('?')[0]) unless candidat['blog'].nil? or candidat['blog'].empty?
+			youtube=html_escape(candidat['youtube'].split('?')[0]) unless candidat['youtube'].nil? or candidat['youtube'].empty?
+			instagram=html_escape(candidat['instagram'].split('?')[0]) unless candidat['instagram'].nil? or candidat['instagram'].empty?
+			wikipedia=html_escape(candidat['wikipedia'].split('?')[0]) unless candidat['wikipedia'].nil? or candidat['wikipedia'].empty?
+			birthday=Date.parse(candidat['birthday'].split('?')[0]) unless candidat['birthday'].nil?
+			age=nil
+			unless birthday.nil? then
+				now = Time.now.utc.to_date
+				age = now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+			end
+			date_verified=Date.parse(candidat['date_verified']) unless candidat['date_verified'].nil?
+			days_verified = (Date.today-date_verified).to_i unless date_verified.nil?
+
 			m=(candidat['gender']=="M")
 			gender={
 				"le"=>m ? "le":"la",
@@ -92,13 +122,48 @@ END
 			else
 				candidat['photo']="https://bot.democratech.co/static/images/missing-photo-M.jpg"
 			end
-			erb :candidat, :locals=>{:candidat=>candidat, :gender=>gender}
+			erb :candidat, :locals=>{
+				:candidat=>candidat,
+				:gender=>gender,
+				:secteur=>secteur,
+				:departement_name=>departement_name,
+				:job=>job,
+				:vision=>vision,
+				:prio1=>prio1,
+				:prio2=>prio2,
+				:prio3=>prio3,
+				:trello=>trello,
+				:website=>website,
+				:facebook=>facebook,
+				:twitter=>twitter,
+				:linkedin=>linkedin,
+				:blog=>blog,
+				:youtube=>youtube,
+				:instagram=>instagram,
+				:wikipedia=>wikipedia,
+				:age=>age,
+				:days_verified=>days_verified,
+			}
 		end
 
 		get '/admin/:candidate_key' do
+			soutiens=[]
 			begin
 				Candidat.db_init()
 				res=Candidat.db_query("get_candidate_by_key",[params['candidate_key']])
+				candidat=res[0]
+				res1=Candidat.db_query("get_supporters_by_key",[params['candidate_key']])
+				if not res1.num_tuples.zero? then
+					res1.each do |r|
+						soutiens.push({
+							'firstname'=>r['firstname'],
+							'lastname'=>r['lastname'],
+							'departement'=>r['departement'],
+							'city'=>r['city'],
+							'support_date'=>Date.parse(r['support_date']).to_s
+						})
+					end
+				end
 			rescue PG::Error => e
 				status 500
 				return erb :error, :locals=>{:error=>{"title"=>"Erreur serveur","message"=>e.message}}
@@ -109,26 +174,26 @@ END
 				status 404
 				return erb :error, :locals=>{:error=>{"title"=>"Page candidat inconnue","message"=>"Cette page ne correspond à aucun candidat"}}
 			end
-			candidat=res[0]
+			email=html_escape(candidat['email']) unless candidat['email'].nil?
 			secteur=html_escape(candidat['secteur']) unless candidat['secteur'].nil?
-			departement_name=candidat['departement'].split(' - ')[1]
-			departement_name=html_escape(departement_name) unless candidat['departement'].nil?
+			departement_name=candidat['departement'].split(' - ')[1] unless candidat['departement'].nil?
+			departement_name=html_escape(departement_name) unless departement_name.nil?
 			departement=html_escape(candidat['departement']) unless candidat['departement'].nil?
 			job=html_escape(candidat['job']) unless candidat['job'].nil?
-			email=html_escape(candidat['email']) unless candidat['email'].nil?
 			vision=html_escape(candidat['vision']) unless candidat['vision'].nil?
 			prio1=html_escape(candidat['prio1']) unless candidat['prio1'].nil?
 			prio2=html_escape(candidat['prio2']) unless candidat['prio2'].nil?
 			prio3=html_escape(candidat['prio3']) unless candidat['prio3'].nil?
-			trello=html_escape(candidat['trello']) unless candidat['trello'].nil?
-			website=html_escape(candidat['website']) unless candidat['website'].nil?
-			facebook=html_escape(candidat['facebook']) unless candidat['facebook'].nil?
-			twitter=html_escape(candidat['twitter']) unless candidat['twitter'].nil?
-			linkedin=html_escape(candidat['linkedin']) unless candidat['linkedin'].nil?
-			blog=html_escape(candidat['blog']) unless candidat['blog'].nil?
-			instagram=html_escape(candidat['instagram']) unless candidat['instagram'].nil?
-			wikipedia=html_escape(candidat['wikipedia']) unless candidat['wikipedia'].nil?
-			birthday=Date.parse(candidat['birthday']) unless candidat['birthday'].nil?
+			trello=html_escape(candidat['trello'].split('?')[0]) unless candidat['trello'].nil? or candidat['trello'].empty?
+			website=html_escape(candidat['website'].split('?')[0]) unless candidat['website'].nil? or candidat['website'].empty?
+			facebook=html_escape(candidat['facebook'].split('?')[0]) unless candidat['facebook'].nil? or candidat['facebook'].empty?
+			twitter=html_escape(candidat['twitter'].split('?')[0]) unless candidat['twitter'].nil? or candidat['twitter'].empty?
+			linkedin=html_escape(candidat['linkedin'].split('?')[0]) unless candidat['linkedin'].nil? or candidat['linkedin'].empty?
+			blog=html_escape(candidat['blog'].split('?')[0]) unless candidat['blog'].nil? or candidat['blog'].empty?
+			youtube=html_escape(candidat['youtube'].split('?')[0]) unless candidat['youtube'].nil? or candidat['youtube'].empty?
+			instagram=html_escape(candidat['instagram'].split('?')[0]) unless candidat['instagram'].nil? or candidat['instagram'].empty?
+			wikipedia=html_escape(candidat['wikipedia'].split('?')[0]) unless candidat['wikipedia'].nil? or candidat['wikipedia'].empty?
+			birthday=Date.parse(candidat['birthday'].split('?')[0]) unless candidat['birthday'].nil?
 			age=nil
 			unless birthday.nil? then
 				now = Time.now.utc.to_date
@@ -153,7 +218,7 @@ END
 				'photo'=>"Field3=#{candidat['candidate_key']}&Field4=#{candidat['email']}",
 				'about'=>"Field15=#{candidat['candidate_key']}&Field18=#{email}&Field8=#{candidat['birthday']}&Field12=#{secteur}&Field17=#{job}&Field9=#{departement}",
 				'summary'=>"Field6=#{candidat['candidate_key']}&Field7=#{email}&Field1=#{vision}&Field3=#{prio1}&Field2=#{prio2}&Field4=#{prio3}",
-				'links'=>"Field8=#{trello}&Field1=#{website}&Field2=#{facebook}&Field3=#{twitter}&Field4=#{linkedin}&Field5=#{blog}&Field6=#{instagram}&Field7=#{wikipedia}&Field9=#{candidat['candidate_key']}&Field11=#{email}"
+				'links'=>"Field13=#{youtube}&Field8=#{trello}&Field1=#{website}&Field2=#{facebook}&Field3=#{twitter}&Field4=#{linkedin}&Field5=#{blog}&Field6=#{instagram}&Field7=#{wikipedia}&Field9=#{candidat['candidate_key']}&Field11=#{email}"
 			}
 			erb :admin, :locals=>{
 				:candidat=>candidat,
@@ -173,10 +238,12 @@ END
 				:twitter=>twitter,
 				:linkedin=>linkedin,
 				:blog=>blog,
+				:youtube=>youtube,
 				:instagram=>instagram,
 				:wikipedia=>wikipedia,
 				:age=>age,
-				:days_verified=>days_verified
+				:days_verified=>days_verified,
+				:soutiens=>soutiens
 			}
 		end
 
