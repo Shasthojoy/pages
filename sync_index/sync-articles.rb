4439c404236bd53eef77c8310c4c2c66422c5124
Thibauld Favre <tfavre@gmail.com>
2016-09-10 03:33:44 -0400

    Ajout filtres algolia sur pages thematiques

--- sync_index/sync-articles.rb
+++ sync_index/sync-articles.rb
@@ -18,7 +18,7 @@ db=PG.connect(
 Algolia.init :application_id=>ALGOLIA_ID, :api_key=>ALGOLIA_KEY
 index_articles=Algolia::Index.new("articles")
 articles_list=<<END
-SELECT c.name, c.slug, c.photo, a.*,at.name as theme, at.slug as theme_slug, pat.name as parent_theme, pat.slug as parent_theme_slug 
+SELECT c.name, c.slug, c.photo, a.*,at.name as theme, at.slug as theme_slug, pat.name as parent_theme, pat.slug as parent_theme_slug, date_part('day',now()-a.date_published) as nb_days_published
 FROM articles as a 
 INNER JOIN candidates as c ON (c.candidate_id=a.candidate_id)
 INNER JOIN articles_themes as at ON (at.theme_id=a.theme_id) 
@@ -30,6 +30,7 @@ END
 res=db.exec(articles_list)
 if not res.num_tuples.zero? then
 	res.each do |r|
+		date_published=Date.parse(r['date_published'])
 		index_articles.save_object({
 			"objectID"=>r['article_id'],
 			"slug"=>r['slug'],
@@ -38,7 +39,8 @@ if not res.num_tuples.zero? then
 			"parent_theme"=>r['parent_theme'],
 			"parent_theme_slug"=>r['parent_theme_slug'],
 			"published_url"=>r['published_url'],
-			"date_published"=>r['date_published'],
+			"date_published"=>date_published,
+			"nb_days_published"=>r['nb_days_published'].to_i,
 			"candidate_id"=>r['candidate_id'],
 			"name"=>r['name'],
 			"title"=>r['title'],
